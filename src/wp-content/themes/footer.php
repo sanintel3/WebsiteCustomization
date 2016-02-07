bc7553db1741ea353387fad480c5385ae2085e69
san <sanintel3@gmail.com>
2016-02-07 08:33:09 +0000

    styles customized

---
+++ src/wp-content/themes/footer.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package Sydney
+ */
+?>
+			</div>
+		</div>
+	</div><!-- #content -->
+
+	<?php if ( is_active_sidebar( 'footer-1' ) ) : ?>
+		<?php get_sidebar('footer'); ?>
+	<?php endif; ?>
+
+    <a class="go-top"><i class="fa fa-angle-up"></i></a>
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info container">
+			<span class="text-green">Helping hands are holier than praying lips</span>
+                        <span class="sep text-green"> | </span>
+                        <a href="mailto:sriramakrishnaorganization@gmail.com?subject=Hello!"><span class="text-green">sriramakrishnaorganization@gmail.com</span></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
